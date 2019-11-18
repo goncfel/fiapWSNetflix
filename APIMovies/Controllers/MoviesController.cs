995dc05da82aa558e9289302e431be5f26ea0cdc
Goncalves <Goncalves@GONCALVES-PC>
2019-11-17 22:15:06 -0200

    v1

---
+++ APIMovies/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using APIMovies.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APIMovies.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Details")]
+        public List<Movie> Details()
+        {
+            return _context.Movies.ToList();
+        }
+
+        [HttpGet("DetailsByKeyword")]
+        public List<Movie> DetailsByKeyword(string key)
+        {
+            List<Movie> ret = new List<Movie>();
+            
+            if (_context.Movies.Any(x => x.MovieName.ToUpper().Contains(key.ToUpper()) || x.Description.ToUpper().Contains(key.ToUpper())))
+            {
+                ret = _context.Movies.Where(x => x.MovieName.ToUpper().Contains(key.ToUpper()) || x.Description.ToUpper().Contains(key.ToUpper())).ToList();
+            }
+
+            return ret;
+        }
+
+        // GET: Movies/Details/5
+        [HttpGet("Details/{id}")]
+        public Movie Details(int? id)
+        {
+            if (id == null)
+            {
+                throw new Exception("Parâmetro Inválido");
+            }
+
+            Movie movie = new Movie();
+            if (_context.Movies.Any(m => m.Id == id))
+            {
+                movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+            }
+            else
+            {
+                throw new Exception("Filme não encontrado");
+            }
+
+            return movie;
+        }
+    }
+}
---
+++ APIMovies/Model/Movies.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIMovies.Model
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string Description { get; set; }
+    }
+}
